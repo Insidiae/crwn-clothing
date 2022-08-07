2f52609ecd52cf357df28a8c4ebf4d034a10fc2d
Kobe Ruado <insidiae423@gmail.com>
2022-08-08 02:33:34 +0800

    Add Vite PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,42 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), svgr()],
+	plugins: [
+		react(),
+		svgr(),
+		VitePWA({
+			registerType: "autoUpdate",
+			manifest: {
+				short_name: "CRWN Clothing",
+				name: "ZTM React Milestone Project: CRWN Clothing",
+				icons: [
+					{
+						src: "favicon.ico",
+						sizes: "64x64 32x32 24x24 16x16",
+						type: "image/x-icon",
+					},
+					{
+						src: "crwn-192x192.png",
+						type: "image/png",
+						sizes: "192x192",
+						purpose: "any maskable",
+					},
+					{
+						src: "crwn-512x512.png",
+						type: "image/png",
+						sizes: "512x512",
+						purpose: "any maskable",
+					},
+				],
+				start_url: ".",
+				display: "standalone",
+				theme_color: "#000000",
+				background_color: "#ffffff",
+			},
+		}),
+	],
 });
